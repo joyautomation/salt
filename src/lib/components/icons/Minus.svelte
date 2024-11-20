51d0e14c745df57af9b637dc59139c423511de58
James Joy <joyja@joyautomation.com>
2024-11-19 21:36:31 -0800

    added minus icon

---
+++ src/lib/components/icons/Minus.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let size = '1.5rem'
+</script>
+
+<svg
+	style:height={size}
+	style:width={size}
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="1.5"
+	stroke="currentColor"
+	class="size-6"
+>
+	<path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+</svg>
