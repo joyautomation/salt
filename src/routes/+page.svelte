5ba885ad567275df5b6825dd78520ce3fc7083f4
James A. Joy, PE <38839768+joyja@users.noreply.github.com>
2025-02-24 19:09:34 -0800

    added style to dev testing and fixed so we don't get all these dart errors (#4)
    * added style to dev testing and fixed so we don't get all these dart errors
    
    * 0.0.16
    
    ---------
    
    Co-authored-by: James A Joy <joyja@mira1.internal1.jarautomation.io>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script>
+	import '$lib/styles/main.scss'
 	import { enhance } from '$app/forms'
 	import Toast from '$lib/components/Toast.svelte'
 </script>
 
-<form method="post" action="?/click" use:enhance>
-	<button>Click me</button>
+<form class="m-10 flex align-center" method="post" action="?/click" use:enhance>
+	<button class="button--primary">Click me</button>
 </form>
 <Toast />
-
-<style>
-	button {
-		background-color: var(--color-primary);
-		color: var(--color-white);
-		padding: 1rem 2rem;
-		border-radius: 0.5rem;
-	}
-</style>
