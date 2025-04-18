3505a3e195264c52a6108202f8f79f33e455f6e1
James A. Joy, PE <38839768+joyja@users.noreply.github.com>
2025-04-18 16:40:38 -0700

    added generic icon and svg folders filled with hero-icons for now (#7)
    Co-authored-by: James A Joy <joyja@mira1.internal1.jarautomation.io>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,6 @@ export * as client from "./client.js";
 export { default as Toggle } from "./components/Toggle.svelte";
 export { default as ThemeButton } from "./components/ThemeButton.svelte";
 export { default as Toast } from "./components/Toast.svelte";
+export { default as Icon } from "./components/Icon.svelte";
 export * as icons from "./components/icons/index.js";
 export * as state from "./state/notifications.svelte";
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,11 @@
 	import '$lib/styles/main.scss'
 	import { enhance } from '$app/forms'
 	import Toast from '$lib/components/Toast.svelte'
+	import Icon from '../lib/components/Icon.svelte'
 </script>
 
 <form class="m-10 flex align-center" method="post" action="?/click" use:enhance>
 	<button class="button--primary">Click me</button>
 </form>
+<Icon path="outline/academic-cap" size={48} />
 <Toast />
